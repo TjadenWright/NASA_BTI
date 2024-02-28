3986571a9c2e7c496105a5e56d341a211b728516
TjadenWright <tjaden.wright@jacks.sdstate.edu>
2024-02-27 18:47:35 -0600

    added controls for lenovo legion go. Need to test this on prototype rover.

--- Controls/run_controls_classV2.py
+++ Controls/run_controls_classV2.py
@@ -21,20 +21,20 @@ prev_Select = 0
 controller_numb = 0                        # <--- controller # used.
 VERBOSE = False                            # <--- do you want diagnostic data?
 baud_rate = 9600                           # <--- make this the same as the arduino
-PC_or_PI = "PC"                            # <--- PC or pi?
+PC_or_PI = "Lenovo"                            # <--- PC or pi?
 
 # setup the rover controls class.
 rc1 = Rover_Controls(verbose=VERBOSE, PC_or_PI = PC_or_PI)
 rc1.setup_USB_Controller(controller_numb=controller_numb) # pass in the controller # you want to use (default = 0)
 
 # setup communication with the arduino
-if(PC_or_PI == "PC"):
-    rc1.Enable_Write_arduino(baud_rate = baud_rate, arduino_name = 'Latte')
+if(PC_or_PI == "Lenovo"):
+    rc1.Enable_Write_arduino(baud_rate = baud_rate, arduino_name = 'Arduino')
 else:
     rc1.Enable_Write_arduino(baud_rate = baud_rate, arduino_name = 'ACM')
 
 # run the code for manual and automatic.
-while not rc1.Get_Button_From_Controller():            # keep getting data till the manual control button has been pressed (defaults to PS Home Button).
+while not rc1.Get_Button_From_Controller("Menu"):            # keep getting data till the manual control button has been pressed (defaults to PS Home Button).
     # get the button to change mode
     Select = rc1.Get_Button_From_Controller(stop_button="Select") # select button is pressed or not
 
@@ -49,8 +49,9 @@ while not rc1.Get_Button_From_Controller():            # keep getting data till 
 
     # different modes (manual vs auto)
     if(mode == 0): # manual mode
-        rc1.Write_message(data=rc1.Controller_To_PWM_and_DIR()) # send PWM data to the arduino
-        print(str(rc1.Controller_To_PWM_and_DIR())  + " Manual Mode")
+
+        rc1.Write_message(data=rc1.Motor_PWM_controller()) # send PWM data to the arduino
+        print(str(rc1.Motor_PWM_controller())  + " Manual Mode")
 
     if(mode == 1): # auto mode
         Direction = 0
--- Python Class/Controls_ClassV2.py
+++ Python Class/Controls_ClassV2.py
@@ -83,52 +83,62 @@ class Rover_Controls:
         return [left_x, left_y, right_x, right_y, dpad_up, dpad_down, dpad_left, dpad_right, L2_trigger, R2_trigger, buttons[0],
                 buttons[1], buttons[3], buttons[2], buttons[9], buttons[10], buttons[4], buttons[6], buttons[5], buttons[8], buttons[7]]
 
-    # def USB_Controller_PC(self):
-    #     pygame.event.get()  # Get pygame events
+    def USB_Controller_Lenovo(self):
+        pygame.event.get()  # Get pygame events
 
-    #     # Get joystick axes
-    #     left_x = self.joystick.get_axis(0)
-    #     left_y = self.joystick.get_axis(1)
-    #     right_x = self.joystick.get_axis(2)
-    #     right_y = self.joystick.get_axis(5)
+        # Get joystick axes
+        left_x = self.joystick.get_axis(0)
+        left_y = self.joystick.get_axis(1)
+        right_x = self.joystick.get_axis(2)
+        right_y = self.joystick.get_axis(3) # self.joystick.get_axis(5)
 
-    #     # Get D-pad buttons
-    #     hat = self.joystick.get_hat(0)
-    #     dpad_up = max(0, hat[1])
-    #     dpad_down = max(0, -hat[1])
-    #     dpad_left = max(0, -hat[0])
-    #     dpad_right = max(0, hat[0])
+        # Get D-pad buttons
+        hat = self.joystick.get_hat(0)
+        dpad_up = max(0, hat[1])
+        dpad_down = max(0, -hat[1])
+        dpad_left = max(0, -hat[0])
+        dpad_right = max(0, hat[0])
 
-    #     # Get trigger analog
-    #     L2_trigger = self.joystick.get_axis(3)
-    #     R2_trigger = self.joystick.get_axis(4)
+        # Get trigger analog
+        L2_trigger = self.joystick.get_axis(4) # self.joystick.get_axis(3)
+        R2_trigger = self.joystick.get_axis(5) # self.joystick.get_axis(4)
 
-    #     # Get joystick buttons using list comprehension
-    #     buttons = [self.joystick.get_button(i) for i in range(self.joystick.get_numbuttons())]
+        # Get joystick buttons using list comprehension
+        buttons = [self.joystick.get_button(i) for i in range(self.joystick.get_numbuttons())]
 
-    #     # if you want to debug your values set verbose to True
-    #     if self.verbose:
-    #         print(f"Left Stick (X, Y): ({left_x}, {left_y})")
-    #         print(f"Right Stick (X, Y): ({right_x}, {right_y})")
-    #         print(f"Buttons: {buttons}")
-    #         print(f"DPAD: {dpad_up, dpad_down, dpad_left, dpad_right}")
-    #         print(f"Triggers: {L2_trigger, R2_trigger}")
-    #         time.sleep(1)
+        # if you want to debug your values set verbose to True
+        if self.verbose:
+            print(f"Left Stick (X, Y): ({left_x}, {left_y})")
+            print(f"Right Stick (X, Y): ({right_x}, {right_y})")
+            print(f"Buttons: {buttons}")
+            print(f"DPAD: {dpad_up, dpad_down, dpad_left, dpad_right}")
+            print(f"Triggers: {L2_trigger, R2_trigger}")
+            time.sleep(1)
 
-    #     # switch 2 and 3 to match pi
-    #     temp = buttons[2]
-    #     buttons[2] = buttons[3]
-    #     buttons[3] = temp
+        # controls[0] = Left_Joystick_X
+        # controls[1] = Left_Joystick_Y
+        # controls[2] = Right_Joystick_X
+        # controls[3] = Right_Joystick_Y
+        # controls[4] = Dpad_Up
+        # controls[5] = Dpad_Down
+        # controls[6] = Dpad_Left
+        # controls[7] = Dpad_Right
+        # controls[8] = L2_Trigger
+        # controls[9] = R2_Trigger
+        # controls[10] = A_Button (button[0])
+        # controls[11] = B_Button (button[1])
+        # controls[12] = Y_Button (buttons[3])
+        # controls[13] = X_Button (button[2])
+        # controls[14] = L1_Button (buttons[4])
+        # controls[15] = R1_Button (buttons[5])
+        # controls[16] = Menu (buttons[7])
+        # controls[17] = Select (buttons[6])
+        # controls[18] = Left_Stick_In (buttons[8])
+        # controls[19] = Right_Stick_In (buttons[9])
 
-    #     # switch 8 and 10, then 10 and 9 to match pi
-    #     temp = buttons[8]
-    #     buttons[8] = buttons[10]
-    #     buttons[10] = buttons[9]
-    #     buttons[9] = temp
-
-    #     # return controller inputs for that pass-through
-    #     return [left_x, left_y, right_x, right_y, dpad_up, dpad_down, dpad_left, dpad_right, L2_trigger, R2_trigger, buttons[2],
-    #             buttons[1], buttons[0], buttons[3], buttons[10], buttons[11], buttons[4], buttons[7], buttons[12], buttons[5], buttons[6]]
+        # return controller inputs for that pass-through
+        return [left_x, left_y, right_x, right_y, dpad_up, dpad_down, dpad_left, dpad_right, L2_trigger, R2_trigger, buttons[0],
+                buttons[1], buttons[3], buttons[2], buttons[4], buttons[5], buttons[7], buttons[6], buttons[8], buttons[9]]
 
     def USB_Controller_PI(self):
         pygame.event.get()  # Get pygame events
@@ -175,6 +185,8 @@ class Rover_Controls:
     def Motor_PWM_controller(self):
         if(self.PC_or_PI == "PC"):
             self.controls = self.USB_Controller_PC() # get controller input
+        elif(self.PC_or_PI == "Lenovo"):
+            self.controls = self.USB_Controller_Lenovo()
         else:
             self.controls = self.USB_Controller_PI() # get controller input
 
@@ -217,6 +229,8 @@ class Rover_Controls:
     def Controller_To_PWM_and_DIR(self):
         if(self.PC_or_PI == "PC"):
             self.controls = self.USB_Controller_PC() # get controller input
+        elif(self.PC_or_PI == "Lenovo"):
+            self.controls = self.USB_Controller_Lenovo()
         else:
             self.controls = self.USB_Controller_PI() # get controller input
 
@@ -284,6 +298,8 @@ class Rover_Controls:
         #if self.controls is None:
         if(self.PC_or_PI == "PC"):
             self.controls = self.USB_Controller_PC()
+        elif(self.PC_or_PI == "Lenovo"):
+            self.controls = self.USB_Controller_Lenovo()
         else:
             self.controls = self.USB_Controller_PI()
 
@@ -291,6 +307,33 @@ class Rover_Controls:
         return Home_Button
     
     def Controller_Button_Map(self, button_name):
+        # controls[0] = Left_Joystick_X
+        # controls[1] = Left_Joystick_Y
+        # controls[2] = Right_Joystick_X
+        # controls[3] = Right_Joystick_Y
+        # controls[4] = Dpad_Up
+        # controls[5] = Dpad_Down
+        # controls[6] = Dpad_Left
+        # controls[7] = Dpad_Right
+        # controls[8] = L2_Trigger
+        # controls[9] = R2_Trigger
+        # controls[10] = A_Button (button[0])
+        # controls[11] = B_Button (button[1])
+        # controls[12] = Y_Button (buttons[3])
+        # controls[13] = X_Button (button[2])
+        # controls[14] = L1_Button (buttons[4])
+        # controls[15] = R1_Button (buttons[5])
+        # controls[16] = Menu (buttons[7])
+        # controls[17] = Select (buttons[6])
+        # controls[18] = Left_Stick_In (buttons[8])
+        # controls[19] = Right_Stick_In (buttons[9])
+
+        if(self.PC_or_PI == "Lenovo"):
+            Controls_Names = ['Left_Joystick_X', 'Left_Joystick_Y', 'Right_Joystick_X', 'Right_Joystick_Y',
+                          'Dpad_Up', 'Dpad_Down', 'Dpad_Left', 'Dpad_Right', 'L2_Trigger', 'R2_Trigger',
+                          'A_Button', 'B_Button', 'Y_Button', 'X_Button', 'L1_Button', 'R1_Button',
+                          'Menu', 'Select', 'Left_Stick_In', 'Right_Stick_In']
+        
         # controls[0] = Left_Joystick_X
         # controls[1] = Left_Joystick_Y
         # controls[2] = Right_Joystick_X
@@ -312,10 +355,11 @@ class Rover_Controls:
         # controls[18] = PS_Logo
         # controls[19] = Left_Stick_In
         # controls[20] = Right_Stick_In // ps logo back 2
-
-        Controls_Names = ['Left_Joystick_X', 'Left_Joystick_Y', 'Right_Joystick_X', 'Right_Joystick_Y',
-                          'Dpad_Up', 'Dpad_Down', 'Dpad_Left', 'Dpad_Right', 'L2_Trigger', 'R2_Trigger',
-                          'X_Button', 'O_Button', 'Triangle_Button', 'Square_Button', 'L1_Button', 'R1_Button',
-                          'Select', 'Start', 'PS_Logo', 'Left_Stick_In', 'Right_Stick_In']
+        
+        else:
+            Controls_Names = ['Left_Joystick_X', 'Left_Joystick_Y', 'Right_Joystick_X', 'Right_Joystick_Y',
+                            'Dpad_Up', 'Dpad_Down', 'Dpad_Left', 'Dpad_Right', 'L2_Trigger', 'R2_Trigger',
+                            'X_Button', 'O_Button', 'Triangle_Button', 'Square_Button', 'L1_Button', 'R1_Button',
+                            'Select', 'Start', 'PS_Logo', 'Left_Stick_In', 'Right_Stick_In']
         
         return Controls_Names.index(button_name)
