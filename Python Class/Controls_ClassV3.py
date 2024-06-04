f9d9372af089d8a116bfbb28202adaa370143961
TjadenWright <tjaden.wright@jacks.sdstate.edu>
2024-06-04 18:15:53 -0500

    dtr

--- Python Class/Controls_ClassV3.py
+++ Python Class/Controls_ClassV3.py
@@ -452,7 +452,8 @@ class Rover_Controls:
         print(arduino_port)
         # Set up the serial connection
         self.arduino[index] = serial.Serial(arduino_port, baudrate=baud_rate) # connection made.
-        # self.arduino[index].timeout = 0.1
+        self.arduino[index].timeout = 0.1
+        self.arduino[index].dtr = True
         time.sleep(10)
 
     # def Disable_write_arduino(self, index = 0):
